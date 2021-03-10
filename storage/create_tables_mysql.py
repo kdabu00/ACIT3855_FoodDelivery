67f636c1a6064c1e5a7e4c3ab130b52d3d80c5dd
Kevin J. Dabu <55458444+kdabu00@users.noreply.github.com>
2021-03-10 13:08:16 -0800

    Update create_tables_mysql.py

--- storage/create_tables_mysql.py
+++ storage/create_tables_mysql.py
@@ -1,5 +1,5 @@
 import mysql.connector
-db_conn = mysql.connector.connect(host="mysql-food-delivery-app-lab6.eastus2.cloudapp.azure.com", user="root", password="P@ssw0rd", database="events")
+db_conn = mysql.connector.connect(host="mysql-food-delivery-app-lab6.eastus2.cloudapp.azure.com", user="root", password=, database="events")
 db_cursor = db_conn.cursor()
 db_cursor.execute('''
           CREATE TABLE pickup
