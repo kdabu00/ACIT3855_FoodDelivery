9ab116f2aaee5f4d8354e85203a1385cc6c4f70e
Kevin J. Dabu <55458444+kdabu00@users.noreply.github.com>
2021-03-31 19:30:29 -0700

    Update app.py

--- audit_log/app.py
+++ audit_log/app.py
@@ -11,15 +11,27 @@ from flask_cors import CORS, cross_origin
 
 headers = {"Content-Type": "application/json"}
 
-with open('app_conf.yml', 'r') as f:
-    app_config = yaml.safe_load(f.read())
+if "TARGET_ENV" in os.environ and os.environ["TARGET_ENV"] == "test":
+   print("In Test Environment")
+   app_conf_file = "/config/app_conf.yml"
+   log_conf_file = "/config/log_conf.yml"
+else:
+   print("In Dev Environment")
+   app_conf_file = "app_conf.yml"
+   log_conf_file = "log_conf.yml"
 
-with open('log_conf.yml', 'r') as f:
-    log_config = yaml.safe_load(f.read())
-    logging.config.dictConfig(log_config)
+with open(app_conf_file, 'r') as f:
+   app_config = yaml.safe_load(f.read())
+
+# External Logging Configuration
+with open(log_conf_file, 'r') as f:
+   log_config = yaml.safe_load(f.read())
+   logging.config.dictConfig(log_config)
 
 logger = logging.getLogger('basicLogger')
 
+logger.info("App Conf File: %s" % app_conf_file)
+logger.info("Log Conf File: %s" % log_conf_file)
 
 def get_pickup_order(index):
     """ Get PO Reading in History """
