ddedfc014f81c47905d4328ff6d8d2105f9093ff
Kevin J. Dabu <55458444+kdabu00@users.noreply.github.com>
2021-03-31 19:31:13 -0700

    Update app.py

--- receiver/app.py
+++ receiver/app.py
@@ -10,14 +10,27 @@ from pykafka import KafkaClient
 
 headers = {"Content-Type": "application/json"}
 
-with open('app_conf.yml', 'r') as f:
-    app_config = yaml.safe_load(f.read())
+if "TARGET_ENV" in os.environ and os.environ["TARGET_ENV"] == "test":
+   print("In Test Environment")
+   app_conf_file = "/config/app_conf.yml"
+   log_conf_file = "/config/log_conf.yml"
+else:
+   print("In Dev Environment")
+   app_conf_file = "app_conf.yml"
+   log_conf_file = "log_conf.yml"
 
-with open('log_conf.yml', 'r') as f:
-    log_config = yaml.safe_load(f.read())
-    logging.config.dictConfig(log_config)
+with open(app_conf_file, 'r') as f:
+   app_config = yaml.safe_load(f.read())
+
+# External Logging Configuration
+with open(log_conf_file, 'r') as f:
+   log_config = yaml.safe_load(f.read())
+   logging.config.dictConfig(log_config)
 
 logger = logging.getLogger('basicLogger')
+
+logger.info("App Conf File: %s" % app_conf_file)
+logger.info("Log Conf File: %s" % log_conf_file)
 
 
 def report_pickup_order(body):
